210f085f43eac00e3dc5e5b7fe252ec073dc3a37
ahmad miqdad <miqdaddeveloper@gmail.com>
2023-12-27 13:47:46 +0700

    [feat] : Add ImageConstant

---
+++ lib/config/constant/image_constant.dart
@@ -0,0 +1,7 @@
+class ImageConstant {
+  // Image folder path
+  static String imagePath = 'assets/images';
+
+  // Splash images
+  static String imgWelcomePage = '$imagePath/img_welcome_page.png';
+}
