6b463ff0fa6c93e4e12e4827237594ade94c32a0
ahmad miqdad <miqdaddeveloper@gmail.com>
2023-12-27 13:45:50 +0700

    [feat] : Add CustomButtonWidget

---
+++ lib/common_widget/button/custom_button.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:online_course_app/config/theme/app_theme.dart';
+
+import 'base_button.dart';
+
+class CustomButtonWidget extends BaseButton {
+  const CustomButtonWidget({
+    super.key,
+    this.decoration,
+    this.leftIcon,
+    this.rightIcon,
+    EdgeInsets? margin,
+    VoidCallback? onPressed,
+    ButtonStyle? buttonStyle,
+    Alignment? alignment,
+    TextStyle? buttonTextStyle,
+    bool? isDisabled,
+    double? height,
+    double? width,
+    required String text,
+  }) : super(
+          text: text,
+          onPressed: onPressed,
+          buttonStyle: buttonStyle,
+          isDisabled: isDisabled,
+          buttonTextStyle: buttonTextStyle,
+          height: height,
+          width: width,
+          alignment: alignment,
+          margin: margin,
+        );
+
+  final BoxDecoration? decoration;
+
+  final Widget? leftIcon;
+
+  final Widget? rightIcon;
+
+  @override
+  Widget build(BuildContext context) {
+    return alignment != null
+        ? Align(
+            alignment: alignment ?? Alignment.center,
+            child: buildElevatedButtonWidget,
+          )
+        : buildElevatedButtonWidget;
+  }
+
+  Widget get buildElevatedButtonWidget => Container(
+        height: height ?? 52,
+        width: width ?? double.maxFinite,
+        margin: margin,
+        decoration: decoration,
+        child: ElevatedButton(
+          style: buttonStyle,
+          onPressed: isDisabled ?? false ? null : onPressed ?? () {},
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              leftIcon ?? const SizedBox.shrink(),
+              Text(
+                text,
+                style: buttonTextStyle ??
+                    theme.textTheme.titleMedium,
+              ),
+              rightIcon ?? const SizedBox.shrink(),
+            ],
+          ),
+        ),
+      );
+}
