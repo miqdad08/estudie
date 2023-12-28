da70d4728e73e3140a620510ea51ca719cdbb34a
ahmad miqdad <miqdaddeveloper@gmail.com>
2023-12-28 08:20:42 +0700

    [feat] : Add Custom Icon Button

---
+++ lib/common_widget/icon/custom_icon_button.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+
+import '../../config/theme/app_theme.dart';
+
+class CustomIconButton extends StatelessWidget {
+  const CustomIconButton({
+    Key? key,
+    this.alignment,
+    this.height,
+    this.width,
+    this.padding,
+    this.decoration,
+    this.child,
+    this.onTap,
+  }) : super(
+          key: key,
+        );
+
+  final Alignment? alignment;
+
+  final double? height;
+
+  final double? width;
+
+  final EdgeInsetsGeometry? padding;
+
+  final BoxDecoration? decoration;
+
+  final Widget? child;
+
+  final VoidCallback? onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return alignment != null
+        ? Align(
+            alignment: alignment ?? Alignment.center,
+            child: iconButtonWidget,
+          )
+        : iconButtonWidget;
+  }
+
+  Widget get iconButtonWidget => SizedBox(
+        height: height ?? 0,
+        width: width ?? 0,
+        child: IconButton(
+          padding: EdgeInsets.zero,
+          icon: Container(
+            height: height ?? 0,
+            width: width ?? 0,
+            padding: padding ?? EdgeInsets.zero,
+            decoration: decoration ??
+                BoxDecoration(
+                  color: theme.colorScheme.errorContainer,
+                  borderRadius: BorderRadius.circular(27),
+                ),
+            child: child,
+          ),
+          onPressed: onTap,
+        ),
+      );
+}
+
+extension IconButtonStyleHelper on CustomIconButton {
+  static BoxDecoration get gradientOnPrimaryContainerToOnPrimaryContainer =>
+      BoxDecoration(
+        borderRadius: BorderRadius.circular(25),
+        gradient: LinearGradient(
+          begin: Alignment(0.5, 0),
+          end: Alignment(0.5, 1),
+          colors: [
+            theme.colorScheme.onPrimaryContainer,
+            theme.colorScheme.onPrimaryContainer.withOpacity(0.1),
+          ],
+        ),
+      );
+}
