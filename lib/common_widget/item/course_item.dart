f8b41f2b44083d2f2566ed354d5b7db1aab9424c
ahmad miqdad <miqdaddeveloper@gmail.com>
2024-01-23 10:05:55 +0700

    [feat] : Fix CourseItem

--- lib/common_widget/item/course_item.dart
+++ lib/common_widget/item/course_item.dart
@@ -1,91 +1,77 @@
 import 'package:flutter/material.dart';
-import 'package:online_course_app/common_util/size/methods.dart';
 import 'package:online_course_app/common_widget/image/custom_image.dart';
-import 'package:online_course_app/feature/home/domain/entities/course.dart';
 
+import '../../common_util/size/methods.dart';
 import '../../config/constant/image_constant.dart';
-import '../../config/theme/app_decoration.dart';
 import '../../config/theme/app_theme.dart';
-import '../../config/theme/custom_text_style.dart';
+import '../../feature/course/domain/entities/course.dart';
 
 class CourseItem extends StatelessWidget {
   final Course course;
 
-  const CourseItem({
-    super.key,
-    required this.course,
-  });
+  const CourseItem({super.key, required this.course});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: AppDecoration.fillErrorContainer.copyWith(
-        borderRadius: BorderRadiusStyle.roundedBorder16,
+      height: MediaQuery.of(context).size.width / 2.5,
+      padding: const EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        color: theme.colorScheme.errorContainer,
+        borderRadius: BorderRadius.circular(20),
       ),
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           CustomImageWidget(
+            width: 100,
+            height: 150,
+            radius: BorderRadius.circular(12),
             imagePath: course.posterPath,
-            height: 160,
-            radius: const BorderRadius.vertical(
-              top: Radius.circular(12),
+            fit: BoxFit.cover,
+          ),
+          horizontalSpace(10),
+          _buildTitleAndDescription(),
+        ],
+      ),
+    );
+  }
+
+  Expanded _buildTitleAndDescription() {
+    return Expanded(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            course.title + course.title,
+            maxLines: 3,
+            overflow: TextOverflow.ellipsis,
+            style: theme.textTheme.titleMedium,
+          ),
+          Expanded(
+            child: Text(
+              course.category,
+              style: const TextStyle(
+                fontSize: 12,
+                fontWeight: FontWeight.bold,
+              ),
             ),
           ),
-          verticalSpace(16),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
+          Row(
             children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  SizedBox(
-                    width: 148,
-                    child: Text(
-                      course.title,
-                      maxLines: 2,
-                      overflow: TextOverflow.ellipsis,
-                      style: CustomTextStyles.titleMediumBold.copyWith(
-                        color:
-                            theme.colorScheme.onPrimaryContainer.withOpacity(1),
-                      ),
-                    ),
-                  ),
-                  CustomImageWidget(
-                    imagePath: IconsConstants.icRating,
-                    height: 20,
-                    width: 20,
-                    margin: const EdgeInsets.only(
-                      left: 4,
-                      top: 13,
-                      bottom: 16,
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(
-                      left: 2,
-                      top: 12,
-                      bottom: 16,
-                    ),
-                    child: Text(
-                      '${course.rating}/5',
-                      style: theme.textTheme.titleSmall!.copyWith(
-                        color:
-                            theme.colorScheme.onPrimaryContainer.withOpacity(1),
-                      ),
-                    ),
-                  ),
-                ],
+              CustomImageWidget(
+                imagePath: IconsConstants.icRating,
+                height: 20,
+                width: 20,
               ),
-              verticalSpace(4),
+              horizontalSpace(5),
               Text(
-                course.category,
-                style: theme.textTheme.bodyMedium!.copyWith(
-                  color: theme.colorScheme.onPrimary,
+                '${course.rating}/5',
+                style: theme.textTheme.titleSmall!.copyWith(
+                  color: theme.colorScheme.onPrimaryContainer.withOpacity(1),
                 ),
               ),
-              verticalSpace(16),
             ],
           ),
         ],
