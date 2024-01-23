925b7c532b9ed3ff72a24b3110fd4bb342742c09
ahmad miqdad <miqdaddeveloper@gmail.com>
2024-01-23 10:16:37 +0700

    [feat] : Add Navigator to detail course

--- lib/feature/course/presentation/home_page.dart
+++ lib/feature/course/presentation/home_page.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:online_course_app/common_util/size/methods.dart';
 import 'package:online_course_app/config/constant/image_constant.dart';
-import 'package:online_course_app/feature/home/domain/entities/teacher.dart';
-import 'package:online_course_app/feature/home/presentation/methods/list_course.dart';
 
 import '../../../common_widget/item/course_category_item.dart';
-import '../domain/entities/course.dart';
+import '../../detail_course/presentation/detail_course_page.dart';
+import '../../detail_teacher/presentation/detail_teacher_page.dart';
 import 'methods/course_category.dart';
 import 'methods/last_progress.dart';
+import 'methods/list_course.dart';
 import 'methods/list_teacher.dart';
 import 'methods/search_bar.dart';
 import 'methods/user_info.dart';
@@ -22,30 +23,6 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   String selectedCategory = "";
 
-  final List<Course> courses = [
-    Course(
-      id: 1,
-      title: 'Full-Stack Mobile App Developer',
-      category: 'Coding',
-      rating: 4,
-      posterPath: ImageConstant.imgPlaceholder,
-    ),
-    Course(
-      id: 1,
-      title: 'Design Thinking: Improve Startups Better 100x',
-      category: 'Design',
-      rating: 3,
-      posterPath: ImageConstant.imgPlaceholder,
-    ),
-    Course(
-      id: 1,
-      title: 'Analyze UX for Digital Marketing',
-      category: 'Design',
-      rating: 4,
-      posterPath: ImageConstant.imgPlaceholder,
-    ),
-  ];
-
   final List<CategoryModel> categories = [
     CategoryModel(
       title: 'Design',
@@ -58,27 +35,6 @@ class _HomePageState extends State<HomePage> {
     CategoryModel(
       title: 'Coding',
       icon: IconsConstants.icCode,
-    ),
-  ];
-
-  final List<Teacher> teachers = [
-    Teacher(
-      name: 'Bejo Urang',
-      email: '-',
-      specialist: 'Manager',
-      photoUrl: ImageConstant.imgPlaceholder,
-    ),
-    Teacher(
-      name: 'Nek Tua',
-      email: '-',
-      specialist: 'Manager',
-      photoUrl: ImageConstant.imgPlaceholder,
-    ),
-    Teacher(
-      name: 'Mas Coki',
-      email: '-',
-      specialist: 'Manager',
-      photoUrl: ImageConstant.imgPlaceholder,
     ),
   ];
 
@@ -100,7 +56,6 @@ class _HomePageState extends State<HomePage> {
                 categories: categories,
                 isSelected: selectedCategory,
                 onTap: (String title) {
-                  print(title);
                   setState(() {
                     selectedCategory = title;
                   });
@@ -108,13 +63,15 @@ class _HomePageState extends State<HomePage> {
               ),
               verticalSpace(24),
               listCourse(
-                courses: courses,
-                onTap: (title) {},
+                onTap: (title) {
+                  context.goNamed(DetailCoursePage.routeName, extra: title);
+                },
               ),
               verticalSpace(24),
               listTeacher(
-                teachers: teachers,
-                onTap: (String name) {},
+                onTap: (teacher) {
+                  context.goNamed(DetailTeacherPage.routeName, extra: teacher);
+                },
               ),
               verticalSpace(100),
             ],
