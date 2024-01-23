1c130dba0dfc85e4de891731ef4f5e95f1d74809
ahmad miqdad <miqdaddeveloper@gmail.com>
2024-01-23 10:14:43 +0700

    [fix] : Add FirebaseDetailCourseService

---
+++ lib/feature/detail_course/data/data_source/firebase_detail_course_service.dart
@@ -0,0 +1,164 @@
+import 'dart:io';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:online_course_app/core/resources/result.dart';
+
+import 'package:online_course_app/feature/detail_course/data/model/detail_course.dart';
+import 'package:online_course_app/feature/detail_course/data/model/discussion.dart';
+import 'package:online_course_app/feature/detail_course/data/model/review.dart';
+import 'package:path/path.dart';
+
+import '../model/section.dart';
+import 'detail_course_data_source.dart';
+
+class FirebaseDetailCourseService implements DetailDataSource {
+  final FirebaseFirestore _firebaseFirestore;
+
+  FirebaseDetailCourseService({FirebaseFirestore? firebaseFirestore})
+      : _firebaseFirestore = firebaseFirestore ?? FirebaseFirestore.instance;
+
+  @override
+  Future<Result<DetailCourseModel>> getDetailCourse(String id) async {
+    DocumentReference<Map<String, dynamic>> documentReference =
+        _firebaseFirestore.doc('details/$id');
+    try {
+      DocumentSnapshot<Map<String, dynamic>> result =
+          await documentReference.get();
+      if (result.exists) {
+        return Result.success(DetailCourseModel.fromJson(result.data()!));
+      } else {
+        return const Result.failed('Detail not found');
+      }
+    } on FirebaseException catch (e) {
+      return Result.failed(e.message ?? 'Detail not found');
+    }
+  }
+
+  @override
+  Future<Result<List<ReviewModel>>> getReviews(String id) async {
+    DocumentReference<Map<String, dynamic>> documentReference =
+        _firebaseFirestore.doc('reviews/$id');
+    try {
+      DocumentSnapshot<Map<String, dynamic>> result =
+          await documentReference.get();
+      if (result.exists) {
+        return Result.success(List<ReviewModel>.from(
+            result["reviews"]!.map((item) => ReviewModel.fromJson(item))));
+      } else {
+        return const Result.failed('Reviews not found');
+      }
+    } on FirebaseException catch (e) {
+      return Result.failed(e.message ?? 'Reviews not found');
+    }
+  }
+
+  @override
+  Future<Result<List<SectionModel>>> getLessons(String id) async {
+    DocumentReference<Map<String, dynamic>> documentReference =
+        _firebaseFirestore.doc('lessons/$id');
+    try {
+      DocumentSnapshot<Map<String, dynamic>> result =
+          await documentReference.get();
+      if (result.exists) {
+        return Result.success(List<SectionModel>.from(
+            result["lessons"]!.map((item) => SectionModel.fromJson(item))));
+      } else {
+        return const Result.failed('Lessons not found');
+      }
+    } on FirebaseException catch (e) {
+      return Result.failed(e.message ?? 'Lessons not found');
+    }
+  }
+
+  @override
+  Future<Result<List<DiscussionModel>>> getDiscussions(String id) async {
+    DocumentReference<Map<String, dynamic>> documentReference =
+        _firebaseFirestore.doc('discussions/$id');
+    try {
+      DocumentSnapshot<Map<String, dynamic>> result =
+          await documentReference.get();
+
+      if (result.exists) {
+        return Result.success(List<DiscussionModel>.from(result["discussions"]!
+            .map((item) => DiscussionModel.fromJson(item))));
+      } else {
+        return const Result.success([]);
+      }
+    } on FirebaseException catch (e) {
+      return Result.failed(e.message ?? 'Discussions not found');
+    }
+  }
+
+  @override
+  Future<Result<DiscussionModel>> createDiscussion(
+    DiscussionModel discussion,
+    String id,
+  ) async {
+    CollectionReference<Map<String, dynamic>> discussions =
+        _firebaseFirestore.collection('discussions');
+    try {
+      await discussions.doc(id).set({
+        'discussions': FieldValue.arrayUnion([
+          discussion.toJson(),
+        ]),
+      }, SetOptions(merge: true));
+      var result = await discussions.doc(id).get();
+      if (result.exists) {
+        return Result.success(discussion);
+      } else {
+        return const Result.failed('Failed to create discussion data');
+      }
+    } on FirebaseException catch (e) {
+      return Result.failed(e.message ?? 'Failed to create discussion data ');
+    }
+  }
+
+  @override
+  Future<Result<List<String>>> uploadPhotoAttachments({
+    required List<File> attachmentsFile,
+    required String id,
+  }) async {
+    try {
+      List<String> fileNames =
+          attachmentsFile.map((photo) => basename(photo.path)).toList();
+      List<Reference> references = fileNames
+          .map((fileName) => FirebaseStorage.instance.ref().child(fileName))
+          .toList();
+      List<String> urlPhotos = [];
+      for (int i = 0; i < attachmentsFile.length; i++) {
+        await references[i].putFile(attachmentsFile[i]);
+        String downloadUrl = await references[i].getDownloadURL();
+        urlPhotos.add(downloadUrl);
+      }
+
+      return Result.success(urlPhotos);
+    } catch (e) {
+      return const Result.failed('Failed to upload photo attachments');
+    }
+  }
+
+  @override
+  Future<Result<ReviewModel>> createReview(
+    ReviewModel review,
+    String id,
+  ) async {
+    CollectionReference<Map<String, dynamic>> discussions =
+        _firebaseFirestore.collection('reviews');
+    try {
+      await discussions.doc(id).set({
+        'reviews': FieldValue.arrayUnion([
+          review.toJson(),
+        ]),
+      }, SetOptions(merge: true));
+      var result = await discussions.doc(id).get();
+      if (result.exists) {
+        return Result.success(review);
+      } else {
+        return const Result.failed('Failed to create review data');
+      }
+    } on FirebaseException catch (e) {
+      return Result.failed(e.message ?? 'Failed to create review data');
+    }
+  }
+}
