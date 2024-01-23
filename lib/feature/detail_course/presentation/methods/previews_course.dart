f56fee045fed6a3ba764f71dd83aebe354aa2ba6
ahmad miqdad <miqdaddeveloper@gmail.com>
2024-01-23 10:20:59 +0700

    [feat] : Add PreviewCourse

---
+++ lib/feature/detail_course/presentation/methods/previews_course.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:online_course_app/common_widget/image/custom_image.dart';
+import 'package:online_course_app/common_widget/video_player/video_player_widget.dart';
+
+import '../../../../common_widget/icon/custom_icon_button.dart';
+import '../../../../config/constant/image_constant.dart';
+import '../../domain/entities/video.dart';
+
+Widget previewCourse(
+  BuildContext context, {
+  required String previewImage,
+  required Video previewVideo,
+}) =>
+    GestureDetector(
+      onTap: () {
+        context.goNamed(VideoPlayerView.routeName, extra: previewVideo.videoUrl);
+        // ModalVideoPlayer.show(
+        //   context,
+        //   videoUrl: previewVideo.videoUrl,
+        // );
+      },
+      child: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 24),
+        height: 200,
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(16),
+              child: CustomImageWidget(
+                imagePath: previewImage,
+                width: double.infinity,
+                fit: BoxFit.cover,
+              ),
+            ),
+            CustomIconButton(
+              height: 50,
+              width: 50,
+              padding: const EdgeInsets.all(13),
+              decoration: IconButtonStyleHelper
+                  .gradientOnPrimaryContainerToOnPrimaryContainer,
+              alignment: Alignment.center,
+              child: CustomImageWidget(
+                imagePath: IconsConstants.icPlay,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
