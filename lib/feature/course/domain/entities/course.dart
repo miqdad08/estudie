2d146409af9017f1a837458e7872d83584d94f03
ahmad miqdad <miqdaddeveloper@gmail.com>
2024-01-23 10:02:50 +0700

    [feat] : Add CourseEntity

--- lib/feature/course/domain/entities/course.dart
+++ lib/feature/course/domain/entities/course.dart
@@ -1,15 +1,19 @@
 import 'package:equatable/equatable.dart';
 
 class Course extends Equatable {
-  final int id;
+  final String id;
+  final String idTeacher;
   final String title;
+  final int? price;
   final String category;
-  final int rating;
+  final double rating;
   final String? posterPath;
 
   const Course({
     required this.id,
+    required this.idTeacher,
     required this.title,
+    this.price = 0,
     this.posterPath,
     required this.category,
     required this.rating,
