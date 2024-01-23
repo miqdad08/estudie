c5388b915411a98d7e9e6686262682d5a6687176
ahmad miqdad <miqdaddeveloper@gmail.com>
2024-01-23 10:28:51 +0700

    [feat] : Add Teeacher Event

---
+++ lib/feature/course/presentation/teacher_bloc/teacher_event.dart
@@ -0,0 +1,10 @@
+part of 'teacher_bloc.dart';
+
+abstract class TeacherEvent extends Equatable{
+  const TeacherEvent();
+
+  @override
+  List<Object?> get props => [];
+}
+
+class GetTeachers extends TeacherEvent{}
