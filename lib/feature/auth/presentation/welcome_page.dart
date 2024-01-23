5f06b5eabc0134a797fecf8fff9636f8d7bd5372
ahmad miqdad <miqdaddeveloper@gmail.com>
2024-01-23 10:32:58 +0700

    [fix] : Implement Welcome page

--- lib/feature/auth/presentation/welcome_page.dart
+++ lib/feature/auth/presentation/welcome_page.dart
@@ -1,56 +1,77 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
+import 'package:online_course_app/common_util/extensions/build_context_extension.dart';
+import 'package:online_course_app/common_util/size/methods.dart';
+import 'package:online_course_app/feature/auth/presentation/sign_in_page.dart';
+import 'package:online_course_app/feature/auth/presentation/sign_up_page.dart';
+import 'package:online_course_app/feature/main/main_page.dart';
 import '../../../common_widget/button/button_text_widget.dart';
 import '../../../common_widget/button/custom_button.dart';
 import '../../../config/theme/app_theme.dart';
+import 'auth_bloc/auth_bloc.dart';
 import 'methods/image_banner.dart';
 import 'methods/sign_text.dart';
 
 class WelcomePage extends StatelessWidget {
   static String routeName = "welcome_page";
+
   const WelcomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: ListView(
-        children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 24),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const SizedBox(height: 14),
-                imageBanner(context),
-                const SizedBox(height: 26),
-                signText(),
-                const SizedBox(height: 20),
-                Text(
-                  "New way to study abroad from the real professional with great work.",
-                  maxLines: 2,
-                  overflow: TextOverflow.ellipsis,
-                  style: theme.textTheme.bodyLarge!.copyWith(
-                    height: 2.00,
+    return BlocListener<AuthBloc, AuthState>(
+      listener: (context, state) {
+        if (state is AuthSuccess) {
+         context.goNamed(MainPage.routeName);
+        }
+        if (state is AuthFailed) {
+          context.showSnackBar(state.message);
+        }
+      },
+      child: Scaffold(
+        body: ListView(
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 24),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  verticalSpace(14),
+                  imageBanner(context),
+                  verticalSpace(26),
+                  signText(),
+                  verticalSpace(20),
+                  Text(
+                    "New way to study abroad from the real professional with great work.",
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                    style: theme.textTheme.bodyLarge!.copyWith(
+                      height: 2.00,
+                    ),
                   ),
-                ),
-                const SizedBox(height: 35),
-                const CustomButtonWidget(
-                  text: "Create New Account",
-                ),
-                const SizedBox(height: 31),
-                Align(
-                  alignment: Alignment.center,
-                  child: ButtonTextWidget(
-                    onTap: () {
-                      context.goNamed('main_page');
+                  verticalSpace(35),
+                  CustomButtonWidget(
+                    onPressed: () {
+                      context.goNamed(SignUpPage.routeName);
                     },
-                    title: "Sign In to My Account",
+                    title: "Create New Account",
                   ),
-                ),
-              ],
+                  verticalSpace(31),
+                  Align(
+                    alignment: Alignment.center,
+                    child: ButtonTextWidget(
+                      onTap: () {
+                        context.goNamed(SignInPage.routeName);
+                      },
+                      title: "Sign In to My Account",
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
