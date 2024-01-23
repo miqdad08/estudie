23fb04ad987fcb773d91e704b9f35610f389b53e
ahmad miqdad <miqdaddeveloper@gmail.com>
2024-01-23 10:19:37 +0700

    [feat] : Add Integrate BlocProvider & firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,19 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:online_course_app/feature/auth/presentation/auth_bloc/auth_bloc.dart';
 import 'config/routes/route.dart';
 import 'config/theme/app_theme.dart';
+import 'firebase_options.dart';
+import 'injection_container.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  initializeDependencies();
   runApp(const MyApp());
 }
 
@@ -17,13 +27,17 @@ class MyApp extends StatelessWidget {
       statusBarBrightness: Brightness.dark,
       statusBarIconBrightness: Brightness.dark,
     ));
-    return MaterialApp.router(
-      title: 'Flutter Demo',
-      theme: theme,
-      debugShowCheckedModeBanner: false,
-      routeInformationParser: AppRoutes.router.routeInformationParser,
-      routerDelegate: AppRoutes.router.routerDelegate,
-      routeInformationProvider: AppRoutes.router.routeInformationProvider,
+
+    return BlocProvider<AuthBloc>(
+      create: (context) => sl()..add(AuthGetCurrent()),
+      child: MaterialApp.router(
+        title: 'Flutter Demo',
+        theme: theme,
+        debugShowCheckedModeBanner: false,
+        routeInformationParser: AppRoutes.router.routeInformationParser,
+        routerDelegate: AppRoutes.router.routerDelegate,
+        routeInformationProvider: AppRoutes.router.routeInformationProvider,
+      ),
     );
   }
 }
