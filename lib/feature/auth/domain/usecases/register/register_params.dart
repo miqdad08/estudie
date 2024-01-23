603b6cb0ebb6dd90abc894edc8998e4990cf9ae2
ahmad miqdad <miqdaddeveloper@gmail.com>
2024-01-23 10:21:23 +0700

    [feat] : Add RegisterParams

---
+++ lib/feature/auth/domain/usecases/register/register_params.dart
@@ -0,0 +1,13 @@
+class RegisterParams {
+  final String name;
+  final String email;
+  final String password;
+  final String? photoUrl;
+
+  RegisterParams({
+    required this.name,
+    required this.email,
+    required this.password,
+    this.photoUrl,
+  });
+}
