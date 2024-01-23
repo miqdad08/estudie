2d5265125a19bf491f9c8ea9236ad8b0c9b91dd4
ahmad miqdad <miqdaddeveloper@gmail.com>
2024-01-23 10:23:42 +0700

    [feat] : Add signLoginText

--- lib/feature/auth/presentation/methods/sign_text.dart
+++ lib/feature/auth/presentation/methods/sign_text.dart
@@ -23,3 +23,25 @@ RichText signText() {
     textAlign: TextAlign.left,
   );
 }
+
+RichText signLoginText() {
+  return RichText(
+    text: TextSpan(
+      children: [
+        TextSpan(
+          text: "Learn.\n",
+          style: CustomTextStyles.poppinsOnPrimaryContainer30,
+        ),
+        TextSpan(
+          text: "Practice.\n",
+          style: CustomTextStyles.poppinsPrimaryContainer30,
+        ),
+        TextSpan(
+          text: "Earn.",
+          style: CustomTextStyles.poppinsOnPrimaryContainer30,
+        ),
+      ],
+    ),
+    textAlign: TextAlign.left,
+  );
+}
