fc984a5d3adbad2d99ba3ba04a0abf41cfafa71c
ahmad miqdad <miqdaddeveloper@gmail.com>
2024-04-11 00:03:45 +0700

    [feat & fix] : Add section

--- lib/feature/detail_course/data/model/video.dart
+++ lib/feature/detail_course/data/model/video.dart
@@ -1,29 +1,57 @@
 import '../../domain/entities/video.dart';
 
 class VideoModel {
+  final String id;
   final String name;
   final String videoUrl;
   final bool isDone;
+  final String section;
 
   const VideoModel({
+    required this.id,
     required this.isDone,
     required this.name,
     required this.videoUrl,
+    required this.section,
   });
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'name': name,
+      'videoUrl': videoUrl,
+      'isDone': isDone,
+      'section': section,
+    };
+  }
 
   factory VideoModel.fromJson(Map<String, dynamic> json) {
     return VideoModel(
+      id: json["id"],
       name: json["name"],
       videoUrl: json["videoUrl"],
       isDone: json["isDone"],
+      section: json["section"],
+    );
+  }
+
+  factory VideoModel.fromEntity(Video video) {
+    return VideoModel(
+      id: video.id,
+      isDone: video.isDone,
+      name: video.name,
+      videoUrl: video.videoUrl,
+      section: video.section,
     );
   }
 
   Video toEntity() {
     return Video(
+      id: id,
       isDone: isDone,
       name: name,
       videoUrl: videoUrl,
+      section: section,
     );
   }
 }
--- lib/feature/detail_course/domain/entities/video.dart
+++ lib/feature/detail_course/domain/entities/video.dart
@@ -1,16 +1,20 @@
 import 'package:equatable/equatable.dart';
 
 class Video extends Equatable {
+  final String id;
   final String name;
   final String videoUrl;
   final bool isDone;
+  final String section;
 
   const Video({
+    required this.id,
     required this.isDone,
     required this.name,
     required this.videoUrl,
+    required this.section,
   });
 
   @override
-  List<Object?> get props => [name, videoUrl, isDone];
+  List<Object?> get props => [id, name, videoUrl, isDone, section];
 }
