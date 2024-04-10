ac7716140264fb1c675bf07be90c8036d783cba1
ahmad miqdad <miqdaddeveloper@gmail.com>
2024-04-11 00:29:50 +0700

    [feat & fix] : Add GetEnrolledCourseDetailUseCase

--- lib/feature/detail_course/domain/usecases/get_detail_course.dart
+++ lib/feature/detail_course/domain/usecases/get_detail_course.dart
@@ -1,17 +1,18 @@
 import 'package:fpdart/fpdart.dart';
 import 'package:online_course_app/core/resources/failure.dart';
 import 'package:online_course_app/core/usecase/usecase.dart';
+import 'package:online_course_app/feature/course/domain/entities/course.dart';
 import 'package:online_course_app/feature/detail_course/domain/entities/detail_course.dart';
 import 'package:online_course_app/feature/detail_course/domain/repository/detail_course_repository.dart';
 
 class GetDetailCourseUseCase
-    extends UseCase<Either<Failure, DetailCourse>, String> {
+    extends UseCase<Either<Failure, DetailCourse>, Course> {
   final DetailCourseRepository repository;
 
   GetDetailCourseUseCase(this.repository);
 
   @override
-  Future<Either<Failure, DetailCourse>> call({String? params}) async {
+  Future<Either<Failure, DetailCourse>> call({Course? params}) async{
     return await repository.getCourseDetail(params!);
   }
 }
