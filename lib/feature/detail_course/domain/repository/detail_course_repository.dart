a96eb57a25a3c51191693a0968c95ae20dd554cb
ahmad miqdad <miqdaddeveloper@gmail.com>
2024-04-11 00:02:45 +0700

    [feat & fix] : Add onUnlockCourse

--- lib/feature/detail_course/domain/repository/detail_course_repository.dart
+++ lib/feature/detail_course/domain/repository/detail_course_repository.dart
@@ -3,11 +3,12 @@ import 'dart:io';
 import 'package:online_course_app/core/resources/type_defs.dart';
 import 'package:online_course_app/feature/detail_course/domain/entities/discussion.dart';
 
+import '../../../course/domain/entities/course.dart';
 import '../entities/detail_course.dart';
 import '../entities/review.dart';
 
 abstract class DetailCourseRepository {
-  FutureEither<DetailCourse> getCourseDetail(String id);
+  FutureEither<DetailCourse> getCourseDetail(Course course);
 
   FutureEither<Discussion> createDiscussion({
     required Discussion discussion,
@@ -18,5 +19,10 @@ abstract class DetailCourseRepository {
   FutureEither<Review> createReview({
     required Review review,
     required String id,
+  });
+
+  FutureEither<DetailCourse> unlockCourse({
+    required DetailCourse detailCourse,
+    Course? course,
   });
 }
