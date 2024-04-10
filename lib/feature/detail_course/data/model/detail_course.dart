6f03cc3c139e178ec13adf989862c8da9e33937f
ahmad miqdad <miqdaddeveloper@gmail.com>
2024-04-11 00:01:43 +0700

    [feat & fix] : Fix Detail Course Model & Entity

--- lib/feature/detail_course/data/model/detail_course.dart
+++ lib/feature/detail_course/data/model/detail_course.dart
@@ -1,41 +1,55 @@
+import 'package:online_course_app/feature/course/data/models/teacher/teacher.dart';
 import 'package:online_course_app/feature/detail_course/data/model/discussion.dart';
 import 'package:online_course_app/feature/detail_course/domain/entities/detail_course.dart';
 import 'package:online_course_app/feature/detail_course/data/model/review.dart';
-import 'package:online_course_app/feature/detail_course/data/model/section.dart';
 import 'package:online_course_app/feature/detail_course/data/model/video.dart';
 
 class DetailCourseModel {
   final String id;
   final String about;
+  final int price;
   final VideoModel previewVideo;
+  final TeacherModel? teacher;
   final List<ReviewModel> reviews;
-  final List<SectionModel> lessons;
+  // final List<SectionModel> lessons;
+  final List<VideoModel> lessons;
   final List<DiscussionModel>? discussions;
+  final bool isUnlock;
 
   const DetailCourseModel({
     required this.id,
     required this.about,
+    required this.price,
     required this.previewVideo,
+    this.teacher,
     required this.lessons,
     required this.reviews,
     required this.discussions,
+    required this.isUnlock,
   });
 
   DetailCourseModel copyWith({
     String? id,
     String? about,
+    int? price,
     VideoModel? previewVideo,
+    TeacherModel? teacher,
     List<ReviewModel>? reviews,
-    List<SectionModel>? lessons,
+    // List<SectionModel>? lessons,
+    List<VideoModel>? lessons,
     final List<DiscussionModel>? discussions,
+    bool? isUnlock,
   }) {
     return DetailCourseModel(
       id: id ?? this.id,
       about: about ?? this.about,
+      price: price ?? this.price,
       previewVideo: previewVideo ?? this.previewVideo,
+      teacher: teacher ?? this.teacher,
       reviews: reviews ?? this.reviews,
       lessons: lessons ?? this.lessons,
       discussions: discussions ?? this.discussions,
+      isUnlock: isUnlock ?? this.isUnlock,
     );
   }
 
@@ -43,11 +57,12 @@ class DetailCourseModel {
     return DetailCourseModel(
       id: json['id'],
       about: json['about'],
+      price: json['price'],
       previewVideo: VideoModel.fromJson(json["previewVideo"]),
       lessons: json["lessons"] == null
           ? []
-          : List<SectionModel>.from(
-              json['lessons'].map((item) => SectionModel.fromJson(item)),
+          : List<VideoModel>.from(
+              json['lessons'].map((item) => VideoModel.fromJson(item)),
             ),
       reviews: json["reviews"] == null
           ? []
@@ -60,6 +75,45 @@ class DetailCourseModel {
               json["discussions"]!
                   .map((item) => DiscussionModel.fromJson(item)),
             ),
+      isUnlock: json['isUnlock'] ?? false,
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'about': about,
+      'price': price,
+      'previewVideo': previewVideo.toJson(),
+      // 'teacher': teacher.toJson(),
+      'lessons': lessons.map((section) => section.toJson()).toList(),
+      'reviews': reviews.map((review) => review.toJson()).toList(),
+      'discussions':
+          discussions?.map((discussion) => discussion.toJson()).toList(),
+      'isUnlock': isUnlock,
+    };
+  }
+
+  factory DetailCourseModel.fromEntity(DetailCourse detailCourse) {
+    return DetailCourseModel(
+      id: detailCourse.id,
+      price: detailCourse.price!,
+      about: detailCourse.about!,
+      previewVideo: VideoModel.fromEntity(detailCourse.previewVideo),
+      teacher: TeacherModel.fromEntity(detailCourse.teacher),
+      lessons: List<VideoModel>.from(
+        detailCourse.lessons.map((item) => VideoModel.fromEntity(item)),
+      ),
+      reviews: List<ReviewModel>.from(
+        detailCourse.reviews.map((item) => ReviewModel.fromEntity(item)),
+      ),
+      discussions: detailCourse.discussions == null
+          ? null
+          : List<DiscussionModel>.from(
+              detailCourse.discussions!
+                  .map((item) => DiscussionModel.fromEntity(item)),
+            ),
+      isUnlock: detailCourse.isUnlock,
     );
   }
 
@@ -67,10 +121,14 @@ class DetailCourseModel {
     return DetailCourse(
       id: id,
       about: about,
+      price: price,
       previewVideo: previewVideo.toEntity(),
-      lessons: lessons.map((section) => section.toEntity()).toList(),
+      teacher: teacher!.toEntity(),
+      lessons: lessons.map((video) => video.toEntity()).toList(),
       reviews: reviews.map((review) => review.toEntity()).toList(),
-      discussions: discussions?.map((discussion) => discussion.toEntity()).toList(),
+      discussions:
+          discussions?.map((discussion) => discussion.toEntity()).toList(),
+      isUnlock: isUnlock,
     );
   }
 }
--- lib/feature/detail_course/domain/entities/detail_course.dart
+++ lib/feature/detail_course/domain/entities/detail_course.dart
@@ -1,33 +1,70 @@
 import 'package:equatable/equatable.dart';
 import 'package:online_course_app/feature/detail_course/domain/entities/discussion.dart';
 import 'package:online_course_app/feature/detail_course/domain/entities/review.dart';
-import 'package:online_course_app/feature/detail_course/domain/entities/section.dart';
 import 'package:online_course_app/feature/detail_course/domain/entities/video.dart';
+
+import '../../../course/domain/entities/teacher.dart';
 
 class DetailCourse extends Equatable {
   final String id;
   final String? about;
+  final int? price;
   final Video previewVideo;
-  final List<Section> lessons;
+  final Teacher teacher;
+  // final List<Section> lessons;
+  final List<Video> lessons;
   final List<Review> reviews;
   final List<Discussion>? discussions;
+  final bool isUnlock;
 
   const DetailCourse({
     this.about,
     required this.id,
+    this.price,
+    required this.teacher,
     required this.previewVideo,
     required this.lessons,
     required this.reviews,
     this.discussions,
+    this.isUnlock = false,
   });
+
+  DetailCourse copyWith({
+    String? id,
+    String? about,
+    int? price,
+    Video? previewVideo,
+    Teacher? teacher,
+    List<Video>? lessons,
+    // List<Section>? lessons,
+    List<Review>? reviews,
+    List<Discussion>? discussions,
+    bool? isUnlock,
+  }) {
+    return DetailCourse(
+      id: id ?? this.id,
+      about: about ?? this.about,
+      price: price ?? this.price,
+      previewVideo: previewVideo ?? this.previewVideo,
+      teacher: teacher ?? this.teacher,
+      lessons: lessons ?? this.lessons,
+      reviews: reviews ?? this.reviews,
+      discussions: discussions ?? this.discussions,
+      isUnlock: isUnlock ?? this.isUnlock,
+    );
+  }
 
   @override
   List<Object?> get props => [
-        id,
-        about,
-        previewVideo,
-        lessons,
-        reviews,
-        discussions,
-      ];
+    id,
+    about,
+    price,
+    previewVideo,
+    lessons,
+    reviews,
+    discussions,
+    isUnlock
+  ];
+
+
 }
