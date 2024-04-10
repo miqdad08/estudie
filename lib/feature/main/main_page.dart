3f570b250fa42474a976f3ca16c3bfb2c3bbcf78
ahmad miqdad <miqdaddeveloper@gmail.com>
2024-04-11 00:28:44 +0700

    [feat & fix] : Add MultiBlocProvider

--- lib/feature/main/main_page.dart
+++ lib/feature/main/main_page.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:online_course_app/common_widget/bottom_navigation/bottom_nav_bar.dart';
 import 'package:online_course_app/config/constant/image_constant.dart';
 import 'package:online_course_app/config/theme/app_theme.dart';
+import 'package:online_course_app/feature/enrolled_course/presentation/bloc/enrolled_course/enrolled_course_bloc.dart';
+import 'package:online_course_app/feature/enrolled_course/presentation/enrolled_course_page.dart';
 
 import '../../common_widget/bottom_navigation/bottom_nav_bar_item.dart';
+import '../../injection_container.dart';
 import '../auth/presentation/settings_page.dart';
+import '../course/presentation/course_bloc/course_bloc.dart';
 import '../course/presentation/home_page.dart';
 
 class MainPage extends StatefulWidget {
@@ -19,53 +24,63 @@ class MainPage extends StatefulWidget {
 class _MainPageState extends State<MainPage> {
   int selectedPage = 0;
 
-  final routes= [
+  final routes = [
     const Center(child: HomePage(),),
-    Center(child: Text('Library', style: theme.textTheme.titleMedium,),),
+    const Center(child: EnrolledCoursePage()),
     Center(child: Text('Groups', style: theme.textTheme.titleMedium,),),
     const Center(child: SettingsPage(),),
   ];
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(
-        children: [
-          routes[selectedPage],
-          BottomNavBar(
-            items: [
-              BottomNavBarItem(
-                index: 0,
-                isSelected: selectedPage == 0,
-                title: 'Progress',
-                image: IconsConstants.icNavProgress,
-              ),
-              BottomNavBarItem(
-                index: 1,
-                isSelected: selectedPage == 1,
-                title: 'Library',
-                image: IconsConstants.icNavLibrary,
-              ),
-              BottomNavBarItem(
-                index: 2,
-                isSelected: selectedPage == 2,
-                title: 'Groups',
-                image: IconsConstants.icNavGroups,
-              ),
-              BottomNavBarItem(
-                index: 3,
-                isSelected: selectedPage == 3,
-                title: 'Settings',
-                image: IconsConstants.icNavSettings,
-              ),
-            ],
-            onTap: (int index) {
-              selectedPage = index;
-              setState(() {});
-            },
-            selectedIndex: selectedPage,
-          ),
-        ],
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => CourseBloc(sl())..add(GetCourses()),
+        ),
+        BlocProvider(
+          create: (context) => EnrolledCourseBloc(sl())..add(GetEnrolledCourses()),
+        ),
+      ],
+      child: Scaffold(
+        body: Stack(
+          children: [
+            routes[selectedPage],
+            BottomNavBar(
+              items: [
+                BottomNavBarItem(
+                  index: 0,
+                  isSelected: selectedPage == 0,
+                  title: 'Progress',
+                  image: IconsConstants.icNavProgress,
+                ),
+                BottomNavBarItem(
+                  index: 1,
+                  isSelected: selectedPage == 1,
+                  title: 'Library',
+                  image: IconsConstants.icNavLibrary,
+                ),
+                BottomNavBarItem(
+                  index: 2,
+                  isSelected: selectedPage == 2,
+                  title: 'Groups',
+                  image: IconsConstants.icNavGroups,
+                ),
+                BottomNavBarItem(
+                  index: 3,
+                  isSelected: selectedPage == 3,
+                  title: 'Settings',
+                  image: IconsConstants.icNavSettings,
+                ),
+              ],
+              onTap: (int index) {
+                selectedPage = index;
+                setState(() {});
+              },
+              selectedIndex: selectedPage,
+            ),
+          ],
+        ),
       ),
     );
   }
