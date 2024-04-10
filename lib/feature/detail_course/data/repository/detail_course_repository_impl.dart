48113f4fce1eb1eb15e2634487f1e6f539a22d20
ahmad miqdad <miqdaddeveloper@gmail.com>
2024-04-11 00:03:30 +0700

    [feat & fix] : Add resultTeacher & unlockCourse

--- lib/feature/detail_course/data/repository/detail_course_repository_impl.dart
+++ lib/feature/detail_course/data/repository/detail_course_repository_impl.dart
@@ -7,8 +7,10 @@ import 'package:online_course_app/core/resources/type_defs.dart';
 import 'package:online_course_app/feature/detail_course/domain/entities/detail_course.dart';
 import 'package:online_course_app/feature/detail_course/domain/entities/discussion.dart';
 import 'package:online_course_app/feature/detail_course/domain/entities/review.dart';
+import '../../../course/domain/entities/course.dart';
 import '../../domain/repository/detail_course_repository.dart';
 import '../data_source/detail_course_data_source.dart';
+import '../model/detail_course.dart';
 import '../model/discussion.dart';
 import '../model/review.dart';
 
@@ -18,21 +20,23 @@ class DetailCourseRepositoryImpl implements DetailCourseRepository {
   DetailCourseRepositoryImpl(this._firebaseDetailCourseService);
 
   @override
-  FutureEither<DetailCourse> getCourseDetail(String id) async {
-    final result = await _firebaseDetailCourseService.getDetailCourse(id);
-    final resultReviews = await _firebaseDetailCourseService.getReviews(id);
-    final resultLessons = await _firebaseDetailCourseService.getLessons(id);
-    final resultDiscussion =
-        await _firebaseDetailCourseService.getDiscussions(id);
+  FutureEither<DetailCourse> getCourseDetail(Course course) async {
+    final result = await _firebaseDetailCourseService.getDetailCourse(course.id);
+    final resultReviews = await _firebaseDetailCourseService.getReviews(course.id);
+    final resultLessons = await _firebaseDetailCourseService.getLessons(course.id);
+    final resultDiscussion = await _firebaseDetailCourseService.getDiscussions(course.id);
+    final resultTeacher = await _firebaseDetailCourseService.getTeacher(teacherId: course.idTeacher);
     if (result.isSuccess &&
         resultReviews.isSuccess &&
         resultLessons.isSuccess &&
         resultDiscussion.isSuccess) {
       return Right(result.resultValue!
           .copyWith(
-              reviews: resultReviews.resultValue,
-              lessons: resultLessons.resultValue,
-              discussions: resultDiscussion.resultValue)
+        teacher: resultTeacher.resultValue,
+        reviews: resultReviews.resultValue,
+        lessons: resultLessons.resultValue,
+        discussions: resultDiscussion.resultValue,
+      )
           .toEntity());
     } else {
       return Left(Failure(message: result.errorMessage!));
@@ -49,11 +53,10 @@ class DetailCourseRepositoryImpl implements DetailCourseRepository {
 
     if (fileAttachments != null && fileAttachments.isNotEmpty) {
       final uploadAttachmentResult =
-          await _firebaseDetailCourseService.uploadPhotoAttachments(
+      await _firebaseDetailCourseService.uploadPhotoAttachments(
         attachmentsFile: fileAttachments,
         id: id,
       );
-
       if (uploadAttachmentResult.isSuccess) {
         uploadAttachment = uploadAttachmentResult.resultValue;
       } else {
@@ -89,81 +92,21 @@ class DetailCourseRepositoryImpl implements DetailCourseRepository {
       return Left(Failure(message: result.errorMessage!));
     }
   }
-}
 
-// return Right(
-// DetailCourse(
-// id: 'id-${DateTime.now()}',
-// previewVideo:
-// 'https://www.youtube.com/watch?v=GQyWIur03aw&ab_channel=HeyFlutter%E2%80%A4com',
-// lessons: const [
-// Section(
-// sectionName: 'Introduction',
-// videos: [
-// Video(
-// name: 'Tools',
-// videoUrl:
-// 'https://www.youtube.com/watch?v=GQyWIur03aw&ab_channel=HeyFlutter%E2%80%A4com',
-// isDone: true,
-// ),
-// Video(
-// name: 'Software',
-// videoUrl:
-// 'https://www.youtube.com/watch?v=GQyWIur03aw&ab_channel=HeyFlutter%E2%80%A4com',
-// isDone: true,
-// ),
-// Video(
-// name: 'Eat',
-// videoUrl:
-// 'https://www.youtube.com/watch?v=GQyWIur03aw&ab_channel=HeyFlutter%E2%80%A4com',
-// isDone: false,
-// ),
-// ],
-// ),
-// Section(
-// sectionName: 'Learn',
-// videos: [
-// Video(
-// name: 'Flutter',
-// videoUrl:
-// 'https://www.youtube.com/watch?v=GQyWIur03aw&ab_channel=HeyFlutter%E2%80%A4com',
-// isDone: false,
-// ),
-// Video(
-// name: 'Design',
-// videoUrl:
-// 'https://www.youtube.com/watch?v=GQyWIur03aw&ab_channel=HeyFlutter%E2%80%A4com',
-// isDone: false,
-// ),
-// Video(
-// name: 'Null Safety',
-// videoUrl:
-// 'https://www.youtube.com/watch?v=GQyWIur03aw&ab_channel=HeyFlutter%E2%80%A4com',
-// isDone: true,
-// ),
-// ],
-// ),
-// ],
-// reviews: [
-// Review(
-// nameUser: 'Miqdad',
-// review: 'Keren',
-// time: DateTime.now().millisecondsSinceEpoch,
-// rating: 4,
-// ),
-// Review(
-// nameUser: 'Karen',
-// review: 'Cool',
-// time: DateTime.now().millisecondsSinceEpoch,
-// rating: 5,
-// ),
-// Review(
-// nameUser: 'Shizuka',
-// review:
-// 'This is bad course ever cause i didnt get the knowledge on this course',
-// time: DateTime(2023, 12, 22).millisecondsSinceEpoch,
-// rating: 3.5,
-// ),
-// ],
-// ),
-// );
+  @override
+  FutureEither<DetailCourse> unlockCourse(
+      {required DetailCourse detailCourse, Course? course}) async {
+    final result = await _firebaseDetailCourseService.unlockCourse(
+        detailCourse: DetailCourseModel.fromEntity(detailCourse));
+    if (result.isSuccess && course != null) {
+      final resultTeacher = await _firebaseDetailCourseService.getTeacher(teacherId: course.idTeacher);
+      if(resultTeacher.isSuccess){
+        return Right(result.resultValue!.copyWith(teacher: resultTeacher.resultValue).toEntity());
+      }else{
+        return Left(Failure(message: result.errorMessage!));
+      }
+    } else {
+      return Left(Failure(message: result.errorMessage!));
+    }
+  }
+}
