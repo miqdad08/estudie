653f3823f30b606952d666ab759339ad7f1d14ac
ahmad miqdad <miqdaddeveloper@gmail.com>
2024-04-11 00:05:25 +0700

    [feat & fix] : Add

--- lib/feature/detail_course/data/model/section.dart
+++ lib/feature/detail_course/data/model/section.dart
@@ -11,13 +11,30 @@ class SectionModel {
     required this.videos,
   });
 
+  Map<String, dynamic> toJson() {
+    return {
+      'sectionName': sectionName,
+      'videos': videos.map((video) => video.toJson()).toList(),
+    };
+  }
+
   factory SectionModel.fromJson(Map<String, dynamic> json) {
     return SectionModel(
       sectionName: json['sectionName'],
       videos: json["videos"] == null
           ? []
           : List<VideoModel>.from(
-          json["videos"]!.map((x) => VideoModel.fromJson(x))),
+        json["videos"].map((x) => VideoModel.fromJson(x)),
+      ),
+    );
+  }
+
+  factory SectionModel.fromEntity(Section section) {
+    return SectionModel(
+      sectionName: section.sectionName,
+      videos: List<VideoModel>.from(
+        section.videos.map((x) => VideoModel.fromEntity(x)),
+      ),
     );
   }
 
