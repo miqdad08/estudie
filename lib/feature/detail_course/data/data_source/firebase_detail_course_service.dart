f0846fb7a4ba3409bcc53bed1c575f5febe8f8e8
ahmad miqdad <miqdaddeveloper@gmail.com>
2024-04-13 01:32:36 +0700

    [feat & fix] : Add unlock course, getTeacher

--- lib/feature/detail_course/data/data_source/firebase_detail_course_service.dart
+++ lib/feature/detail_course/data/data_source/firebase_detail_course_service.dart
@@ -3,13 +3,15 @@ import 'dart:io';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:online_course_app/core/resources/result.dart';
+import 'package:online_course_app/feature/course/data/models/teacher/teacher.dart';
 
 import 'package:online_course_app/feature/detail_course/data/model/detail_course.dart';
 import 'package:online_course_app/feature/detail_course/data/model/discussion.dart';
 import 'package:online_course_app/feature/detail_course/data/model/review.dart';
 import 'package:path/path.dart';
 
-import '../model/section.dart';
+import '../../../course/domain/entities/course.dart';
+import '../model/video.dart';
 import 'detail_course_data_source.dart';
 
 class FirebaseDetailCourseService implements DetailDataSource {
@@ -54,15 +56,15 @@ class FirebaseDetailCourseService implements DetailDataSource {
   }
 
   @override
-  Future<Result<List<SectionModel>>> getLessons(String id) async {
+  Future<Result<List<VideoModel>>> getLessons(String id) async {
     DocumentReference<Map<String, dynamic>> documentReference =
         _firebaseFirestore.doc('lessons/$id');
     try {
       DocumentSnapshot<Map<String, dynamic>> result =
           await documentReference.get();
       if (result.exists) {
-        return Result.success(List<SectionModel>.from(
-            result["lessons"]!.map((item) => SectionModel.fromJson(item))));
+        return Result.success(List<VideoModel>.from(
+            result["lessons"]!.map((item) => VideoModel.fromJson(item))));
       } else {
         return const Result.failed('Lessons not found');
       }
@@ -102,7 +104,7 @@ class FirebaseDetailCourseService implements DetailDataSource {
         'discussions': FieldValue.arrayUnion([
           discussion.toJson(),
         ]),
-      }, SetOptions(merge: true));
+      }, SetOptions(merge: false));
       var result = await discussions.doc(id).get();
       if (result.exists) {
         return Result.success(discussion);
@@ -113,6 +115,30 @@ class FirebaseDetailCourseService implements DetailDataSource {
       return Result.failed(e.message ?? 'Failed to create discussion data ');
     }
   }
+
+  ///Use imgbb for uploading image attachments
+  // @override
+  // Future<Result<List<String>>> uploadPhotoAttachments({
+  //   required List<File> attachmentsFile,
+  //   required String id,
+  // }) async {
+  //   try {
+  //     List<String> fileNames =
+  //         attachmentsFile.map((photo) => basename(photo.path)).toList();
+  //     List<Reference> references = fileNames
+  //         .map((fileName) => FirebaseStorage.instance.ref().child(fileName))
+  //         .toList();
+  //     List<String> urlPhotos = [];
+  //     for (int i = 0; i < attachmentsFile.length; i++) {
+  //       await references[i].putFile(attachmentsFile[i]);
+  //       String downloadUrl = await references[i].getDownloadURL();
+  //       urlPhotos.add(downloadUrl);
+  //     }
+  //     return Result.success(urlPhotos);
+  //   } catch (e) {
+  //     return const Result.failed('Failed to upload photo attachments');
+  //   }
+  // }
 
   @override
   Future<Result<List<String>>> uploadPhotoAttachments({
@@ -131,7 +157,6 @@ class FirebaseDetailCourseService implements DetailDataSource {
         String downloadUrl = await references[i].getDownloadURL();
         urlPhotos.add(downloadUrl);
       }
-
       return Result.success(urlPhotos);
     } catch (e) {
       return const Result.failed('Failed to upload photo attachments');
@@ -159,6 +184,50 @@ class FirebaseDetailCourseService implements DetailDataSource {
       }
     } on FirebaseException catch (e) {
       return Result.failed(e.message ?? 'Failed to create review data');
+    }
+  }
+
+  @override
+  Future<Result<DetailCourseModel>> unlockCourse(
+      {required DetailCourseModel detailCourse, Course? course}) async {
+    try {
+      DocumentReference<Map<String, dynamic>> documentReference =
+          _firebaseFirestore.doc('details/${detailCourse.id}');
+      await documentReference.update({'isUnlock': detailCourse.isUnlock});
+
+      DocumentSnapshot<Map<String, dynamic>> result =
+          await documentReference.get();
+
+      if (result.exists) {
+        DetailCourseModel updatedCourse =
+            DetailCourseModel.fromJson(result.data()!);
+        if (updatedCourse.isUnlock == detailCourse.isUnlock) {
+          return Result.success(updatedCourse);
+        } else {
+          return const Result.failed('Failed to update user Data');
+        }
+      } else {
+        return const Result.failed('Failed to update user Data');
+      }
+    } on FirebaseException catch (e) {
+      return Result.failed(e.message ?? 'Failed to update user Data');
+    }
+  }
+
+  @override
+  Future<Result<TeacherModel>> getTeacher({required String teacherId}) async {
+    DocumentReference<Map<String, dynamic>> documentReference =
+        _firebaseFirestore.doc('teachers/$teacherId');
+    try {
+      DocumentSnapshot<Map<String, dynamic>> result =
+          await documentReference.get();
+      if (result.exists) {
+        return Result.success(TeacherModel.fromJson(result.data()!));
+      } else {
+        return const Result.failed('Detail not found');
+      }
+    } on FirebaseException catch (e) {
+      return Result.failed(e.message ?? 'Detail not found');
     }
   }
 }
